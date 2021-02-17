f09c243796464c1ba3e71d5aa6cee9c4715b7a10
Aaron Fung <4683749+afung206@users.noreply.github.com>
2021-02-17 09:59:35 -0800

    Styling updates and cleanup

--- src/components/Header/HeaderContent.tsx
+++ src/components/Header/HeaderContent.tsx
@@ -4,8 +4,10 @@ import { FunctionComponent } from 'react';
 
 export const Header = () => {
     return (
-      <header>
-        <h1>React Flip Clock</h1>
-      </header>
+      <div className="Header">
+        <header>
+          <h1>React Flip Clock</h1>
+        </header>
+      </div>
     );
 };
